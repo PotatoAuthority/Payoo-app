3c00d7665bf6e62d83b5d16d1f58fbfdbf16ddce
PotatoAuthority <ahsanmanjurul27@gmail.com>
2026-02-23 16:19:20 +0600

    login code added

--- script/login.js
+++ script/login.js
@@ -1,1 +1,19 @@
 console.log('login script connected');
+
+document.getElementById('login-btn').addEventListener('click', function(){
+    const phoneHtml = document.getElementById('phone-input');
+    const phone = phoneHtml.value;
+    console.log(phone);
+
+    const pinHtml = document.getElementById('pin-input');
+    const pin = pinHtml.value;
+
+    if(phone == '01234567891' && pin == '1234'){
+        alert('login success');
+        window.location.assign('./home.html');
+    } 
+    else{
+        alert('wrong credentials!');
+        return;
+    }
+})
