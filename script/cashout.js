e3e2f02da0d9d624cafd748f8c48941fb1a5d992
PotatoAuthority <ahsanmanjurul27@gmail.com>
2026-02-25 03:39:03 +0600

    cashout code added

---
+++ script/cashout.js
@@ -0,0 +1,54 @@
+function p(text){
+    console.log(text);
+}
+p('connected: cashout.js')
+
+
+document.getElementById('cashout-btn').addEventListener('click', function(){
+    //current balance
+    const balanceElement = document.getElementById('balance');
+    const balance = balanceElement.innerText;
+    p(balance);
+    //get cashout balance
+    const cashoutElement = document.getElementById('cashout');
+    const cashout = cashoutElement.value;
+    p(cashout);
+
+    //get number
+    const numElement = document.getElementById('agent');
+    // p(agentNum);
+    const agentNum = numElement.value;
+    p(agentNum);
+
+    //calculated balacne
+    const final_balance = Number(balance) - Number(cashout);
+    p(final_balance);
+
+    //get pin
+    const pinElem = document.getElementById('pin-input');
+    const pin = pinElem.value;
+
+    if(agentNum.length != 11){
+        alert('Invalid Num');
+        return;
+    }
+
+    if(cashout <= 0){
+        alert('Invalid amount');
+        return;
+    }
+
+    if(final_balance < 0){
+        alert('Invalid Balance');
+        return;
+    }
+
+    if(pin == 1111){
+        balanceElement.innerText = final_balance;
+        alert('Cashout Success');
+    }
+    else{
+        alert('Wrong PIN');
+        return;
+    }
+})
